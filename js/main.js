53d47b4d6b3d78e6f8de868decdb94a06aa1cf58
sato-d2x <d2x.k.sato@gmail.com>
2020-12-14 17:18:46 +0900

    getPrefData()の関数化と北海道の人口推移データ取得

--- js/main.js
+++ js/main.js
@@ -18,37 +18,52 @@
     },
     // マウント時
     mounted() {
-      // 全都道府県(名前)  https://opendata.resas-portal.go.jp/docs/api/v1/prefectures.html
-      const prefectures_url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
-      axios
-      .get(prefectures_url, { headers: { 'X-API-KEY': 'Fo5AelLTNBQbzMvA0DH7zcGATzaspMEu0IWlM99I' } })
-      .then(response => (this.prefectures = response.data.result))
-      .catch(error => console.log(error));
-    
-      // 各都道府県の人口数  https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
-      const perYear_url = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear';
-
-      // 人口数の配列定義 , 再代入可能
-      let populationData = new Array();
-
-      axios
-        .get(perYear_url, {
-            headers: { 'X-API-KEY': 'Fo5AelLTNBQbzMvA0DH7zcGATzaspMEu0IWlM99I' },
-            params: {
-                // ここにクエリパラメータ(必須)を指定する
-                prefCode: checkedId1,  //最初に選択された県
-                cityCode: '-', //全て取得
-            }
-        })
-        //populationに人口構成(総人口)のデータをオブジェクトとして返す
-        .then(response => {
-            this.population = response.data.result.data[0].data;
-            for (var i = 0; i < this.population.length; i++) {
-                populationData.push(this.population[i].value);
-            }
-            this.population = this.populationData;
-            console.log(populationData); //データ確認
-        })
+      this.getPrefData();
+      this.getPopulationHokkaidoData(); //test
+    },
+    methods: {
+      // 全都道府県データの取得
+      getPrefData() {
+        // 全都道府県(名前)  https://opendata.resas-portal.go.jp/docs/api/v1/prefectures.html
+        const prefectures_url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
+        axios
+        .get(prefectures_url, { headers: { 'X-API-KEY': 'Fo5AelLTNBQbzMvA0DH7zcGATzaspMEu0IWlM99I' } })
+        .then(response => (this.prefectures = response.data.result))
+        .catch(error => console.log(error));
+      },
+      // (test)北海道の人口推移データ
+      getPopulationHokkaidoData() {
+        const perYear_url_Hokkaido = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=1';
+        axios
+        .get(perYear_url_Hokkaido, { headers: { 'X-API-KEY': 'Fo5AelLTNBQbzMvA0DH7zcGATzaspMEu0IWlM99I' } })
+        .then(response => (this.population = response.data.result))
+        .catch(error => console.log(error));
+      }
     }
+      // ここからコメントアウト
+      // // 各都道府県の人口数  https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+      // const perYear_url = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear';
+
+      // // 人口数の配列定義 , 再代入可能
+      // let populationData = new Array();
+
+      // axios
+      //   .get(perYear_url, {
+      //       headers: { 'X-API-KEY': 'Fo5AelLTNBQbzMvA0DH7zcGATzaspMEu0IWlM99I' },
+      //       params: {
+      //           // ここにクエリパラメータ(必須)を指定する
+      //           prefCode: checkedId1,  //最初に選択された県
+      //           cityCode: '-', //全て取得
+      //       }
+      //   })
+      //   //populationに人口構成(総人口)のデータをオブジェクトとして返す
+      //   .then(response => {
+      //       this.population = response.data.result.data[0].data;
+      //       for (var i = 0; i < this.population.length; i++) {
+      //           populationData.push(this.population[i].value);
+      //       }
+      //       this.population = this.populationData;
+      //       console.log(populationData); //データ確認
+      //   })
   })
 })();
